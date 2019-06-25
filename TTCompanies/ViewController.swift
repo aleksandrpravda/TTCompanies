930ee7d668f104fce6750acc4325a574a2d41c3c
Kolya.p <kolya.p@speedandfunction.com>
2019-06-25 15:16:10 +0300

    Initial Commit

---
+++ TTCompanies/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TTCompanies
+//
+//  Created by Nick on 25/06/2019.
+//  Copyright © 2019 aleksandrpravda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
